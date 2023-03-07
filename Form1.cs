7f539799caf9bc1e2a2fdd1801627ebde8d20f0e
expecto <expeo@vk.com>
2023-03-07 17:48:31 +0300

    main menu added

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace boolean_functions_tasks
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
