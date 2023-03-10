d2f873104efc792d8978e05194ae567faa13b596
expecto <expeo@vk.com>
2023-03-10 20:57:58 +0300

    task2 solution wrong added

--- task2.Designer.cs
+++ task2.Designer.cs
@@ -46,6 +46,7 @@
             this.button1.TabIndex = 15;
             this.button1.Text = "Получить ответ";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label4
             // 
--- task2.cs
+++ task2.cs
@@ -16,5 +16,28 @@ namespace boolean_functions_tasks
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label4.Text = "Остаточная: ";
+            string[] functionStr = textBox1.Text.Split();
+            double[] function = new double[functionStr.Length];
+            for (int i = 0; i < function.Length; i++)
+            {
+                function[i] = double.Parse(functionStr[i]);
+            }
+            int index = int.Parse(textBox2.Text);
+            int bit = int.Parse(textBox3.Text);
+            double residue = bit;
+            for (int i = 0; i < function.Length/2+1; i++)
+            {
+                if (i != index)
+                {
+                    residue -= function[i];
+                }
+            }
+            label4.Text += residue;
+
+        }
     }
 }
