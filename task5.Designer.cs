4527b2c7b6ca26c436cd8258eeaf79d5416d08ef
expecto <expeo@vk.com>
2023-03-12 18:51:20 +0300

    task 5 solution added

--- task5.Designer.cs
+++ task5.Designer.cs
@@ -28,12 +28,98 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "f(x) =";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(61, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(268, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Выберите какие аргументы являются фиктивными";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(64, 122);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(37, 17);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "x1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(64, 145);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(37, 17);
+            this.checkBox2.TabIndex = 3;
+            this.checkBox2.Text = "x2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(64, 168);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(37, 17);
+            this.checkBox3.TabIndex = 4;
+            this.checkBox3.Text = "x3";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(213, 137);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 37);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Проверить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // task5
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(380, 246);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "task5";
             this.Text = "task5";
+            this.Load += new System.EventHandler(this.task5_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.Button button1;
     }
 }
--- task5.cs
+++ task5.cs
@@ -16,5 +16,59 @@ namespace boolean_functions_tasks
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] ff = new int[3];
+            string f = label1.Text.Substring(6);
+            int x = 2;
+            for (int w = 0; w < ff.Length; w++)
+            {
+                int a = 0;
+                string f10 = "";
+                for (int i = a * (1 << x); i < f.Length; i += (1 << x + 1))
+                {
+                    for (int j = 0; j < (1 << x); ++j)
+                    {
+                        f10 += f[j + i];
+                    }
+                }
+                a = 1;
+                string f11 = "";
+                for (int i = a * (1 << x); i < f.Length; i += (1 << x + 1))
+                {
+                    for (int j = 0; j < (1 << x); ++j)
+                    {
+                        f11 += f[j + i];
+                    }
+                }
+                if (f10 == f11)
+                {
+                    ff[w] = 1;
+                }
+                x--;
+            }
+            bool t = true;
+            if (checkBox1.Checked && ff[0] == 1 || !checkBox1.Checked && ff[0] == 0)
+                if(checkBox2.Checked && ff[1] == 1 || !checkBox2.Checked && ff[1] == 0)
+                    if(checkBox3.Checked && ff[2] == 1 || !checkBox3.Checked && ff[2] == 0)
+                    {
+                        MessageBox.Show("Все верно!", "Вы правы!", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                        t = false;
+                    }
+            if(t) MessageBox.Show("Ошибка!", "Вы не правы!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+        }
+
+        private void task5_Load(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            string fx = "";
+            for (int i = 0; i < 8; i++)
+            {
+                fx += rnd.Next(0, 2).ToString();
+            }
+            label1.Text += fx;
+        }
     }
 }
