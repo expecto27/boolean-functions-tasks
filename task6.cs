b43e5fa4a4ad2824d6f731e8e7d53d9904d1dcb0
expecto <expeo@vk.com>
2023-03-16 16:27:39 +0300

    task 6 in process

--- task6.cs
+++ task6.cs
@@ -16,5 +16,31 @@ namespace boolean_functions_tasks
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string fx = label2.Text.Substring(7);
+            label2.Text = fx;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int n = int.Parse(textBox1.Text);
+            label2.Text = "f(x) = ";
+            Random rnd = new Random();
+
+            if (n > 7 || n < 1)
+            {
+                MessageBox.Show("Введите n в промежутке от 1 до 7", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            int k = (1 << n);
+            string fx = "";
+            for (int i = 0; i < k; i++)
+            {
+                fx += rnd.Next(0, 2).ToString();
+            }
+            label2.Text += fx;
+        }
     }
 }
