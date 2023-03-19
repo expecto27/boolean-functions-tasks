a6a6f3c7c7c7dc5fb921e628f054318719dfec61
expecto <expeo@vk.com>
2023-03-19 15:13:34 +0300

    solution 6 added

--- task8.Designer.cs
+++ task8.Designer.cs
@@ -28,12 +28,90 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(64, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введите вектор функции";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 187);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "СДНФ :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(64, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "f(x) = ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(103, 91);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(146, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(103, 184);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(146, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(103, 133);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(146, 38);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Получить СДНФ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // task8
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "task8";
+            this.ClientSize = new System.Drawing.Size(433, 263);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "task8";
+            this.Text = "Задача 8";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
     }
 }
--- task8.cs
+++ task8.cs
@@ -16,5 +16,35 @@ namespace boolean_functions_tasks
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string f = textBox1.Text;
+            char[] arg = { 'X', 'Y', 'Z', 'W', 'T', 'P', 'M' };
+            int n = 0;
+            string ans = "";
+            while ((1 << n) < f.Length) n++; // quantity arguments
+
+            for (int i = 0; i < f.Length; i++)
+            {
+                string BinaryCode = Convert.ToString(i, 2); // binary code int i
+                if (f[i] == '1')
+                {
+                    if (i < f.Length / 2)
+                    {
+                        while (BinaryCode.Length < n) BinaryCode = "0" + BinaryCode;
+                    }
+                    for (int j = 0; j < n; j++)
+                    {
+                        if (BinaryCode[j] == '0') ans += "-" + arg[j];
+                        else ans += arg[j];
+                    }
+                    ans += " v ";
+                }
+            }
+            if (ans[(ans.Length) - 1] == ' ') ans = ans.Substring(0, (ans.Length) - 3);
+            
+            textBox2.Text = ans;
+        }
     }
 }
