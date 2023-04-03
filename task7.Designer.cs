987a8b6b722e18da7c3e5bdbc1eb0b13f5a8b0a5
expecto <expeo@vk.com>
2023-04-03 18:11:20 +0300

    task 7 solution added

--- task7.Designer.cs
+++ task7.Designer.cs
@@ -28,12 +28,104 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(179, 210);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(199, 20);
+            this.textBox2.TabIndex = 14;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(79, 213);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Введите КНФ";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(163, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Введите N";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(250, 49);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 11;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(82, 130);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(296, 61);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Сгенерировать вектор функции от n аргументов";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(82, 251);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(296, 62);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Проверить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(79, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "f(x) = ";
+            // 
+            // task7
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "task7";
+            this.ClientSize = new System.Drawing.Size(488, 378);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Name = "task7";
+            this.Text = "Задача 7";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
     }
 }
--- task7.cs
+++ task7.cs
@@ -16,5 +16,174 @@ namespace boolean_functions_tasks
         {
             InitializeComponent();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int n = int.Parse(textBox1.Text);
+            label2.Text = "f(x) = ";
+            Random rnd = new Random();
+
+            if (n > 7 || n < 1)
+            {
+                MessageBox.Show("Введите n в промежутке от 1 до 7", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            int k = (1 << n);
+            string fx = "";
+            for (int i = 0; i < k; i++)
+            {
+                fx += rnd.Next(0, 2).ToString();
+            }
+            label2.Text += fx;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string f = label2.Text.Substring(7);
+
+            int n = 0;
+
+            while ((1 << n) < f.Length) n++; // quantity arguments
+
+            string tKNF = textBox2.Text;
+
+            string[] KNF = { tKNF };
+
+            if (n != 1)
+            {
+                KNF = tKNF.Split('*');
+            }
+            
+            bool answer = false;
+            bool answer1 = true;
+            bool answerTemp1 = true;
+
+            bool answerTemp = false;
+
+            for (int i = 0; i < f.Length; i++)
+            {
+                string BinaryCode = Convert.ToString(i, 2); // Binary code j
+
+                if (i < f.Length / 2) // добавление нулей в начало набора
+                {
+                    while (BinaryCode.Length < n) BinaryCode = "0" + BinaryCode;
+                }
+                if (f[i] == '0')
+                {
+
+
+                    answerTemp1 = true;
+
+                    for (int j = 0; j < KNF.Length; j++) // цикл по всему КНФ 
+                    {
+                        answerTemp = false;
+
+                        for (int u = 0; u < KNF[j].Length; u++) // цикл по каждому элем. КНФ
+                        {
+                            if (KNF[j][u] == 'X')
+                            {
+                                if (u - 1 >= 0 && KNF[j][u - 1] == '-') answerTemp = answerTemp || (!char_to_bool(BinaryCode[0]));
+                                else answerTemp = answerTemp || (char_to_bool(BinaryCode[0]));
+                            }
+                            else if (KNF[j][u] == 'Y')
+                            {
+                                if (u - 1 >= 0 && KNF[j][u - 1] == '-') answerTemp = answerTemp || (!char_to_bool(BinaryCode[1]));
+                                else answerTemp = answerTemp || (char_to_bool(BinaryCode[1]));
+                            }
+                            else if (KNF[j][u] == 'Z')
+                            {
+                                if (u - 1 >= 0 && KNF[j][u - 1] == '-') answerTemp = answerTemp || (!char_to_bool(BinaryCode[2]));
+                                else answerTemp = answerTemp || (char_to_bool(BinaryCode[2]));
+                            }
+                            else if (KNF[j][u] == 'W')
+                            {
+                                if (u - 1 >= 0 && KNF[j][u - 1] == '-') answerTemp = answerTemp || (!char_to_bool(BinaryCode[3]));
+                                else answerTemp = answerTemp || (char_to_bool(BinaryCode[3]));
+                            }
+                            else if (KNF[j][u] == 'T')
+                            {
+                                if (u - 1 >= 0 && KNF[j][u - 1] == '-') answerTemp = answerTemp || (!char_to_bool(BinaryCode[4]));
+                                else answerTemp = answerTemp || (char_to_bool(BinaryCode[4]));
+                            }
+                            else if (KNF[j][u] == 'P')
+                            {
+                                if (u - 1 >= 0 && KNF[j][u - 1] == '-') answerTemp = answerTemp || (!char_to_bool(BinaryCode[5]));
+                                else answerTemp = answerTemp || (char_to_bool(BinaryCode[5]));
+                            }
+                            else if (KNF[j][u] == 'M')
+                            {
+                                if (u - 1 >= 0 && KNF[j][u - 1] == '-') answerTemp = answerTemp || (!char_to_bool(BinaryCode[6]));
+                                else answerTemp = answerTemp || (char_to_bool(BinaryCode[6]));
+                            }
+                        }
+                        answerTemp1 = answerTemp1 && answerTemp;
+                    }
+                    answer = answer || answerTemp1;
+                }
+                else
+                {
+                    answer1 = false;
+                    answerTemp1 = true;
+                    for (int j = 0; j < KNF.Length; j++) // цикл по всему КНФ 
+                    {
+                        answerTemp = false;
+
+                        for (int u = 0; u < KNF[j].Length; u++) // цикл по каждому элем. КНФ
+                        {
+                            if (KNF[j][u] == 'X')
+                            {
+                                if (u - 1 >= 0 && KNF[j][u - 1] == '-') answerTemp = answerTemp || (!char_to_bool(BinaryCode[0]));
+                                else answerTemp = answerTemp || (char_to_bool(BinaryCode[0]));
+                            }
+                            else if (KNF[j][u] == 'Y')
+                            {
+                                if (u - 1 >= 0 && KNF[j][u - 1] == '-') answerTemp = answerTemp || (!char_to_bool(BinaryCode[1]));
+                                else answerTemp = answerTemp || (char_to_bool(BinaryCode[1]));
+                            }
+                            else if (KNF[j][u] == 'Z')
+                            {
+                                if (u - 1 >= 0 && KNF[j][u - 1] == '-') answerTemp = answerTemp || (!char_to_bool(BinaryCode[2]));
+                                else answerTemp = answerTemp || (char_to_bool(BinaryCode[2]));
+                            }
+                            else if (KNF[j][u] == 'W')
+                            {
+                                if (u - 1 >= 0 && KNF[j][u - 1] == '-') answerTemp = answerTemp || (!char_to_bool(BinaryCode[3]));
+                                else answerTemp = answerTemp || (char_to_bool(BinaryCode[3]));
+                            }
+                            else if (KNF[j][u] == 'T')
+                            {
+                                if (u - 1 >= 0 && KNF[j][u - 1] == '-') answerTemp = answerTemp || (!char_to_bool(BinaryCode[4]));
+                                else answerTemp = answerTemp || (char_to_bool(BinaryCode[4]));
+                            }
+                            else if (KNF[j][u] == 'P')
+                            {
+                                if (u - 1 >= 0 && KNF[j][u - 1] == '-') answerTemp = answerTemp || (!char_to_bool(BinaryCode[5]));
+                                else answerTemp = answerTemp || (char_to_bool(BinaryCode[5]));
+                            }
+                            else if (KNF[j][u] == 'M')
+                            {
+                                if (u - 1 >= 0 && KNF[j][u - 1] == '-') answerTemp = answerTemp || (!char_to_bool(BinaryCode[6]));
+                                else answerTemp = answerTemp || (char_to_bool(BinaryCode[6]));
+                            }
+                            
+                        }
+                        answerTemp1 = answerTemp1 && answerTemp;
+                    }
+                    answer1 = answer1 || answerTemp1;
+                }
+            }
+            if (!answer && answer1) MessageBox.Show("Введенная КНФ является верной", "Поздравляем", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else MessageBox.Show("Введенная КНФ является не верной", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+        }
+
+        private bool char_to_bool(char x)
+        {
+            return !(x == '0');
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
